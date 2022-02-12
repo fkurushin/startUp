70cbbe4f1468b80661f5f705501e529218973839
fkurushin <f.kurushin@ya.ru>
2022-02-12 14:21:44 +0300

    now JSON file is avaliable

--- vkparser.py
+++ vkparser.py
@@ -1,13 +1,26 @@
 import vk  # https://dev.vk.com
 import os
+import json
 import requests
 from tqdm import tqdm
+from langdetect import detect
+
+dev_i = '0'
+session_j = '1'
+num_k = 0
 
 GROUP_ID = '-27685949'  # ID группы или страницы человека(которых парсить) с минусом вначале https://regvk.com/id/
-num_posts = 1000  # Количество постов, число кратное 100
+num_posts = 100  # Количество постов для обработки, число кратное 100
 
-os.system('mkdir photos')
-fo = open('log.txt', 'a')  # Возможно нужно поменять мод
+data = list()  # Сначала сюда зпишу все json словари
+data_k = dict()
+
+# Создание файлов в директории проекта
+os.system('mkdir -p photos')
+os.system('mkdir -p jsons')
+
+# Захожу в директорию где должны храниться изображения проекта
+os.chdir('photos')
 
 for i in tqdm(range(num_posts // 100)):
     # я создал переменную среды со своим access_token, это токен, который можно получить https://vkhost.github.io
@@ -15,10 +28,11 @@ for i in tqdm(range(num_posts // 100)):
     session = vk.Session(os.environ['access_token'])
     api = vk.API(session)
     posts = api.wall.get(owner_id=GROUP_ID, offset=i * 100, count=num_posts, extended=1, v=5.84)
-    # Это словарь, ключи count, items, next_from, profiles, groups
+    # posts - это словарь, ключи count, items, next_from, profiles, groups
     # с параметром extended=1 в posts['items'] возварщаестя массив постов, то что надо!
 
     for post in posts['items']:
+
         # Отбираем не рекламные посты и содержащие только одно приложение
         # if 'attachments' in posts:
         try:
@@ -28,24 +42,37 @@ for i in tqdm(range(num_posts // 100)):
                 # С одним приложением нет надобности в цикле по приложением, берем 0
                 # Индекс 0,1,2,3 в данном случае 3 обозначает качество и размер изображения
                 if post['attachments'][0]['type'] == 'photo':
-                    url = post['attachments'][0]['photo']['sizes'][3]['url']
-                    name = 'photos/' + str(post['id']) + os.path.basename(url)[:15]
-                    r = requests.get(url, allow_redirects=True)
-                    open(name, 'wb').write(r.content)
 
-                    # Запись в файл название изображения и текста поста
-                    # Название изображения
-                    fo.write(name)
-                    fo.write('\t')
-                    # Текст поста
+                    url = post['attachments'][0]['photo']['sizes'][3]['url']
+                    img_name = 'startup_img_' + dev_i + '_' + session_j + '_' + str(num_k) + '.jpg'
+                    r = requests.get(url, allow_redirects=True)
+                    open(img_name, 'wb').write(r.content)
+
                     if post['text'] == '':
-                        fo.write('None')
+                        data_k = {'name': img_name,
+                                  'text': 'None',
+                                  'lang': 'None'
+                                  }
                     else:
-                        if '\n' in post['text']:
-                            fo.write(post['text'].replace('\n', '\t'))
-                    fo.write('\n')
+                        data_k = {'name': img_name,
+                                  'text': post['text'],
+                                  'lang': detect(post['text'])
+                                  }
+
+                    data.append(data_k)
+                    num_k += 1
                 else:
                     continue
+
         except KeyError:
             pass
-fo.close()
+
+# Переход в другую директорию
+os.chdir('..')
+os.chdir('jsons')
+
+# Создание json файлов
+for idx, data_i in enumerate(data):
+    json_name = 'startup_' + dev_i + '_' + session_j + '_' + str(idx) + '.json'
+    with open(json_name, "w") as file:
+        json.dump(data_i, file, ensure_ascii=False)
